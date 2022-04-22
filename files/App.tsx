7171732dd25c48048085ef7ee509c2b510313c03
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2022-04-22 18:40:54 -0400

    Update App.tsx

--- files/App.tsx
+++ files/App.tsx
@@ -1,20 +1,14 @@
-import React, { useState } from "react";
-import { ChakraProvider, Heading } from "@chakra-ui/react";
+import React from "react";
+import { ChakraProvider, Button, Heading } from "@chakra-ui/react";
 import { CustomTheme } from "./Theme";
-import { Button } from "@chakra-ui/button";
+import { AddIcon } from "@chakra-ui/icons";
+
 export default function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
-  };
   return (
     <ChakraProvider theme={CustomTheme}>
       <Heading>Welcome to chakra</Heading>
-      <Button colorScheme="teal" onClick={handleClick}>
-        {count}
-      </Button>
       <Button color="brand.400" variant="outline" borderColor="brand.100">
-        "weird style"
+        weird style
       </Button>
     </ChakraProvider>
   );
